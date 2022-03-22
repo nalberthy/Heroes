a0aabda48aa412981c23bd2a19121ffecb6d08a7
nalberthy <nalberthy@pop-os.localdomain>
2022-03-22 20:10:02 -0300

    feat: method to search for heroes

--- src/heroes/controller/hero.ts
+++ src/heroes/controller/hero.ts
@@ -5,6 +5,11 @@ interface IListHeroesParams {
     limit?: number;
     order?: keyof IHero;
 }
+interface ISearchHeroesParams {
+    query: string;
+    filter?: string;
+}
+
 
 const hero = {
     async listHeroes({order,limit}: IListHeroesParams) {
@@ -27,6 +32,40 @@ const hero = {
             );
         }
         return listHeroes.slice(0, limit)
+    },
+    async searchHeroes({filter, query}: ISearchHeroesParams) {
+        const service = Service.getInstance();
+        const listHeroes = await service.getAllSuperHeroes()        
+        return listHeroes.filter((hero) => {
+            let attributes = [];
+            switch (filter) {
+                case "name":
+                    attributes = [hero.name]
+                    break;
+                case "appearance":
+                    attributes =  [hero.gender, hero.race, hero.height, hero.weight, hero.eyeColor, hero.hairColor]
+                    break;
+                case "biography":
+                    attributes = [hero.fullName, hero.alterEgos, hero.aliases, hero.placeOfBirth, hero.firstAppearance, hero.publisher, hero.alignment]
+                    break;
+                case "work":
+                    attributes = [hero.occupation, hero.base]
+                    break;
+                case "connections":
+                    attributes = [hero.groupAffiliation, hero.relatives]
+                    break;
+                default:
+                    attributes = Object.values(hero)
+            }
+            const result = attributes.filter(
+                (value) => {      
+                    if(value === null) return false;
+                    return value.toString().toLowerCase().indexOf(query.toLowerCase()) != -1;
+                }
+            )
+            if(result.length != 0) return true;
+            return false;
+        })
     }
 }
 
